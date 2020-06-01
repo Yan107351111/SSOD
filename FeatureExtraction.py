4e7d5b2860f833a93236bffef90bde04642af10d
Yaniv Ziselman <yanivzis@campus.technion.ac.il>
2020-06-01 08:53:37 +0300

    redesigning data iteration

--- FeatureExtraction.py
+++ FeatureExtraction.py
@@ -135,16 +135,103 @@ class FrameRegionProposalsDataset(Dataset):
             return features, label, idx
         return features
         
+class FramesDataset(Dataset):
+    def __init__(self, root_dir, label, transform=None, output = 6, random_seed = 0):
+        self.root_dir  = root_dir
+        self.transform = transform
+        self.label     = label
+        self.output    = output
+        torch.manual_seed(random_seed)
+        self.video_ref   = {}
+        self.video_deref = {}
+        self.all_items   = []
+        self.tensors     = []
+        video_hash = 0
+        assert label in os.listdir(root_dir), f'folder {label} not found in the root directory'
         
-class Label(NamedTuple):
-    '''
-    class holding all training assisting data.
-    '''
-    label: int
-    box: List[int]
-    video: int
+        # creating positive item list
+        for i in os.listdir(os.path.join(root_dir, label)):
+            img_path = os.path.join(label, i)
+            image = plt.imread(os.path.join(self.root_dir,img_path))
+            with torch.no_grad():
+                features = self.transform(image)
+            self.all_items.append(img_path)
+            self.tensors.append(features)
+            video_name = i.split(';')[1]
+            if video_name not in list(self.video_ref):
+                self.video_ref[video_name] = video_hash
+                self.video_deref[video_hash] = video_name
+                video_hash+=1
+                
+        # addign negative items to the list
+        other_labels = [olabel for olabel in os.listdir(root_dir)
+                        if olabel is not label]
+        neg_labels   = torch.randint(len(other_labels), (len(self.all_items),))
+        for neg_label in neg_labels:
+            other_label     = other_labels[neg_label]
+            regions         = os.listdir(os.path.join(root_dir,other_label))
+            neg_region_ind  = torch.randint(len(regions), (1,))
+            neg_region_name = os.listdir(
+                os.path.join(root_dir, other_label))[neg_region_ind]
+            video_name = neg_region_name.split(';')[1]
+            neg_region_name = os.path.join(
+                    other_labels[neg_label],
+                    neg_region_name)
+            while neg_region_name in self.all_items:
+                neg_label       = torch.randint(len(other_labels), (1,))
+                other_label     = other_labels[neg_label]
+                regions         = os.listdir(
+                    os.path.join(root_dir,other_label))
+                neg_region_ind  = torch.randint(len(regions), (1,))
+                neg_region_name = os.listdir(
+                    os.path.join(root_dir, other_label))[neg_region_ind]
+                video_name = neg_region_name.split(';')[1]
+                neg_region_name = os.path.join(
+                    other_labels[neg_label],
+                    neg_region_name)
+            image = plt.imread(os.path.join(self.root_dir,neg_region_name))
+            with torch.no_grad():
+                features = self.transform(image)
+            self.all_items.append(neg_region_name)
+            self.tensors.append(features)
+            if video_name not in list(self.video_ref):
+                self.video_ref[video_name] = video_hash
+                self.video_deref[video_hash] = video_name
+                video_hash+=1
 
+        
+        
+    
+    def __len__(self):
+        return len(self.all_items)
 
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+        
+        # print(f'getting item {idx} out of {len(self)}')
+        
+        
+        item = self.all_items[idx]
+        # print(f'item = {item}')
+        # img_name = os.path.join(self.root_dir,item)
+        # image    = plt.imread(img_name)
+        features = self.tensors[idx].squeeze()
+        # image    = image.reshape(1,*image.shape)
+        label    = torch.tensor(1.) if os.path.split(item)[0]==self.label else torch.tensor(0.)
+        video    = torch.tensor(self.video_ref[os.path.split(item)[1].split(';')[1]])
+        box      = torch.tensor([int(i) for i in item.split(';')[3:7]])
+        frame    = torch.tensor(int(item.split(';')[2]))
+        # if self.transform:
+        #     with torch.no_grad():
+        #         features = self.transform(image)
+                # features.requires_grad = False
+        if self.output == 6:
+            return features, label, idx, box, video, frame
+        if self.output == 3:
+            return features, label, idx
+        return features 
+        
 def get_dataloader(data_path, batch_size, label):
     '''
     TODO:
--- SelectiveSearch.py
+++ SelectiveSearch.py
@@ -12,33 +12,72 @@ import os
 import sys
 from tqdm import tqdm
 import time
+from typing import Union, Tuple
 
-if __name__ == '__main__':
-    
-    ### ARGS ###
-    DATA_PATH = sys.argv[1]#'..\\..\\data\\example'
-    OUT_PATH  = sys.argv[2]#'..\\..\\data\\region_proposals'
-    ############ defults
-    N      = 300 # number of reg props
-    SKIP   = 5   # take only SKIP'th proposal
-    LABELS = ['bike', 'cup', 'dog', 'drum', 'guitar',
-               'gun', 'horse', 'pan', 'plate',
-               'scissors', 'tire']
-    IMSIZE = 299
-    ############
-    
+
+def selective_search(
+        data_path: str, out_path: str, label: str,
+        region_num: int = 300, region_skip: int = 3, imsize: Union[int, Tuple] = 299,
+        min_width: int = 30, min_hight: int = 30, min_size: int = 200):
+    '''
+    perform selective search on images in folder "data_path".
+    output rescaled to "imsize" images will be saved at "out_path".
+
+    Parameters
+    ----------
+    data_path : str
+        path leading to data images.
+    out_path : str
+        path the region proposal will be saved to.
+    label : str
+        the label of positive image set.
+    region_num : int, optional
+        Number of total region proposals to take from each image.
+        The default is 300.
+    region_skip : int, optional
+        The number of region proposals to skip for ever saved one.
+        The default is 3.
+    imsize : Union[int, Tuple], optional
+        the size of the resized region proposals.
+        The default is 299.
+    min_width : int, optional
+        the minimal output image width.
+        The default is 30.
+    min_hight : int, optional
+        the minimal output image hight.
+        The default is 30.
+    min_size : int, optional
+        the minimal output image size in pixels.
+        The default is 200.
+
+    Raises
+    ------
+    ValueError
+        DESCRIPTION.
+
+    Returns
+    -------
+    None.
+
+    '''
+    if  isinstance(imsize, int):
+        imsize = (299, 299)
+    elif isinstance(imsize, tuple):
+        pass
+    else:
+        raise ValueError(f'imsize not recognised\n'
+                         'Supposed to be one of: int, tuple\n'
+                         'But got {type(imsize)}')
+    assert region_skip>=0
     
     image_names = [image_name
-                   for image_name in os.listdir(DATA_PATH)
-                   if not image_name.startswith('neg')]
+                   for image_name in os.listdir(DATA_PATH)]
+    
     ss = cv2.ximgproc.segmentation.createSelectiveSearchSegmentation()
     try:
         os.mkdir(OUT_PATH)
     except:pass
-    for label in LABELS:
-        try:
-            os.mkdir(os.path.join(OUT_PATH, label))
-        except:pass 
+    
     count_total = len(image_names)
     count       = 0
     start_time  = time.time()
@@ -48,32 +87,74 @@ if __name__ == '__main__':
             print(f'runing time:{time.time()-start_time}')
             print(f'processed: {count}/{count_total}')
             print(f'remaining time approximately: {(time.time()-start_time)/count*(count_total-count)}')
-        for label in LABELS:
-            if image_name.startswith(label):
-                break
         # process image for reg props
         image = cv2.imread(os.path.join(DATA_PATH, image_name))
         ss.setBaseImage(image)
         ss.switchToSelectiveSearchFast()
         ssresults = ss.process()
         
-        ims    = 0
-        for e,result in enumerate(ssresults):
-            
-            if e%SKIP == 0 and ims<N:
+        saved = 0
+        ims  = 0
+        
+        for init_skip in range(region_skip+1):
+            if ims>=region_num:
+                    break
+            skip = init_skip
+            for e,result in enumerate(ssresults):
+                if skip==init_skip+region_skip+1:
+                    skip = init_skip
+                if skip!=init_skip:
+                    skip+=1
+                    continue
                 x,y,w,h = result
+                # if the region is not up to standards, skip it
+                if w<min_width or h<min_hight or w*h<min_size:
+                    skip+=1
+                    continue
+                # crop, resize and save
                 crop = image[y:y+h, x:x+w]
-                
                 cv2.imwrite(
                     os.path.join(
                         OUT_PATH,
                         label,
                         f'{image_name[:-4]};{x};{y};{w};{h};{ims:04}.png',
                         ),
-                    cv2.resize(crop, (IMSIZE, IMSIZE))
+                    cv2.resize(crop, (imsize, imsize))
                     )
-                
                 ims+=1            
+                skip+=1
+                if ims>=region_num:
+                    break
+        
+
+
+
+
+if __name__ == '__main__':
+    
+    ### ARGS ###
+    DATA_PATH = sys.argv[1]#'..\\..\\data\\example'
+    OUT_PATH  = sys.argv[2]#'..\\..\\data\\region_proposals'
+    ############ defults
+    N      = 300 # number of reg props
+    SKIP   = 3   # take only SKIP'th proposal
+    LABELS = ['bike', 'cup', 'dog', 'drum', 'guitar',
+               'gun', 'horse', 'pan', 'plate',
+               'scissors', 'tire']
+    IMSIZE = 299
+    MIN_W = 30
+    MIN_H = 30
+    MIN_P = 200
+    ############
+    
+    selective_search(
+        DATA_PATH, OUT_PATH, LABELS[0],
+        N, IMSIZE, MIN_W, MIN_H, MIN_P,
+    )
+    
+            
+                    
+                    
         
     
     
